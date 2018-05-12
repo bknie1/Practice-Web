62be29c76191fd1b9bee650715ccaa76b949e3ce
bknie1 <brandonknieriem26@gmail.com>
2018-05-12 00:21:15 -0400

    Added some placeholder DOM interaction, oldschool vs. ES6 Class notes.

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -1,6 +1,21 @@
-var test = 0;
-hw();
+daysAlive();
 
-function hw() {
-  console.log("Hello js.");
+class Person {
+  constructor(name, age, city) {
+    this.name = name;
+    this.age = age;
+    this.city = city;
+  }
+}
+
+var person = {
+  name: "Brandon",
+  age: 28,
+  city: "New Haven"
+};
+
+function daysAlive() {
+  var years = prompt("How many years have you been alive?");
+  var days = years * 365.25;
+  document.getElementById('daysText').textContent = days;
 }
