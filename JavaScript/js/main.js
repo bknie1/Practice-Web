be0e84281b771e46d403f4a5c22385e2658e580d
bknie1 <brandonknieriem26@gmail.com>
2018-05-12 14:37:43 -0400

    Added some game score logic.

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -53,7 +53,57 @@ var header = document.querySelector('h1');
 header.addEventListener('click', function (e) {
   alert("Header clicked!");
 });
+//------------------------------------------------------------------------------
+// Game Score Logic
+var score1 = 0;
+var score2 = 0;
+var gameOver = false;
+var addP1 = document.querySelector('#add-p1');
+var addP2 = document.querySelector('#add-p2');
+var reset = document.querySelector('#reset');
 
+addP1.addEventListener('click', incrementScore('p1'));
+addP2.addEventListener('click', incrementScore('p2'));
+reset.addEventListener('click', resetScore);
+
+function incrementScore(player) {
+  console.log('Increment Score event assigned.');
+    return player == 'p1' ? function increment() {
+      if(!gameOver) {
+        var currentScore = document.querySelector('#score-input').value;
+        score1 += Number(currentScore);
+        console.log('P1 scored.', currentScore);
+        document.querySelector('#p1-score').textContent = score1;
+        console.log("Player 1: " + score1 + " Player 2: " + score2);
+        if(score1 < 5) return;
+        gameOver = true;
+        }
+        document.querySelector('#game-status').textContent = "Game over!";
+
+    } : function increment() {
+      if(!gameOver) {
+        var currentScore = document.querySelector('#score-input').value;
+        console.log('P2 scored.', currentScore);
+        score2 += Number(currentScore);
+        document.querySelector('#p2-score').textContent = score2;
+        console.log("Player 1: " + score1 + " Player 2: " + score2);
+        if(score2 < 5) return;
+        gameOver = true;
+      }
+      document.querySelector('#game-status').textContent = "Game over!";
+    };
+}
+
+function resetScore() {
+  console.log('Resetting game.');
+  score1 = 0;
+  score2 = 0;
+  gameOver = false;
+
+  document.querySelector('#game-status').textContent = "Playing to 5.";
+  document.querySelector('#p1-score').textContent = score1;
+  document.querySelector('#p2-score').textContent = score2;
+}
 //------------------------------------------------------------------------------
 
 // ES6 Class Definition Example
