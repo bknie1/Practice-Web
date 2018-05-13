126beefa447799ef131d1f7bc9590b006b6d8688
bknie1 <brandonknieriem26@gmail.com>
2018-05-12 23:34:01 -0400

    Completed Todo list with animations.

--- Todo List/js/main.js
+++ Todo List/js/main.js
@@ -11,6 +11,11 @@ function assignListeners() {
   and future remove-X that lives under a ul. Below, we have to assign toggle again,
   but we don't have to assign remove again beacuse of this. */
   $('ul').on('click', '.remove-X', removeItem);
+
+  // I did this with jQuery but preferred manually handling the CSS. Smoother.
+  $('.fa-plus').on('click', function(event) {
+    $('.todo-input').toggleClass('toggle-input-off');
+  });
 }
 //------------------------------------------------------------------------------
 function toggleCompleted(event) {
@@ -22,7 +27,6 @@ function removeItem(event) {
   $(this).parent().fadeOut('400', function() {
     $(this).remove(); // this == parent.
   });
-  event.stopPropogation();
 }
 //------------------------------------------------------------------------------
 // Adding list items
@@ -32,7 +36,7 @@ $("input[type='text']").keypress(function(event) {
     // Capture Input text
     var todoText = $('#todo-input').val();
     // Generate new li.
-    $('ul').append('<li><span class="remove-X">X</span>\n' + todoText + '</li>');
+    $('ul').append('<li><span class="remove-X"><i class="fa fa-trash"></i></span>\n' + todoText + '</li>');
     // Add listeners to last/newest list item.
     $('li:last').click(toggleCompleted); // We need this because we used click.
     // $('.remove-X:last').click(removeItem); // We don't need this because of on.
