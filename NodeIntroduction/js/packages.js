3a7043e93ae1cb866f6bb7b525b1d9050a225a1b
bknie1 <brandonknieriem26@gmail.com>
2018-05-13 23:10:01 -0400

    Added faker package example.

--- NodeIntroduction/js/packages.js
+++ NodeIntroduction/js/packages.js
@@ -1,5 +1,18 @@
 const catMe = require('cat-me');
 const kkj = require('knock-knock-jokes');
+const faker = require('faker');
 
 console.log(catMe() + '\n');
 console.log(kkj());
+
+// Generate 10 random product names and 10 random prices.
+console.log('\nWelcome to my shop!');
+
+var fakeCurrency = faker.finance.currencySymbol();
+
+for (var i = 0; i < 10; i++) {
+  var fakeProduct = faker.commerce.product();
+  var fakeAdjective = faker.commerce.productAdjective();
+  var fakePrice = faker.commerce.price();
+  console.log(fakeAdjective + ' ' + fakeProduct + ' - ' + fakeCurrency + ' ' + fakePrice);
+}
