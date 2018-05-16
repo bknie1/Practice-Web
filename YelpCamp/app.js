f1e3a98caf9446e8691af5fa225b4e2df51c67d2
bknie1 <brandonknieriem26@gmail.com>
2018-05-16 11:50:45 -0400

    Added initial YelpCamp project frame.

---
+++ YelpCamp/app.js
@@ -0,0 +1,41 @@
+// REQUIRES --------------------------------------------------------------------
+const express = require('express'); // 'req' snippet.
+var app = express(); // Typical local assignment.
+
+// Parses GET input form data into a usable JavaScript object.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+// We can use this to make API requests.
+const request = require('request');
+// SETUP -----------------------------------------------------------------------
+// DEPENDENCIES
+app.set('view engine', 'ejs'); // 'as' snippet.
+app.use(express.static(__dirname + '/public')); // 'static' snippet.
+
+// LISTEN
+var port = 3000;
+app.listen(port, function () {
+  console.log(`Server Starts on ${port}`);
+});
+// DATABASE --------------------------------------------------------------------
+
+
+// ROUTES ----------------------------------------------------------------------
+// GET REQUESTS -----------------------
+// '/' Root
+app.get('/', (req, res) => {
+  console.log("Someone requested 'root'.");
+  res.render('index');
+});
+//-------------------------------------
+// POST REQUESTS ----------------------
+
+//-------------------------------------
+// DEFAULT
+// '*' Default
+app.get('*', (req, res) => {
+  console.log("Someone requested something that doesn't exist.");
+  res.status(404);
+  res.render('404');
+});
